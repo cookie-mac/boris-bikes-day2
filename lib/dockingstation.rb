0ddccc72005d6a1a29cc902ad7cdfbf40880f793
cookie-mac <ianjmcnicholas@gmail.com>
2021-02-05 15:38:45 +0000

    Chapter 12 finished

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -2,19 +2,17 @@ require_relative './bike'
 
 class Docking_Station
 
-  attr_reader :docked_bike
+  attr_reader :bike
 
 def release_bike
-  if @docked_bike == nil
-    raise "No bikes available"
-  else
-    Bike.new
-  end
+  fail "No bikes available" unless @bike
+  @bike
 end
 
 
 def dock(bike)
-  @docked_bike = bike
+  fail "The docking station is full" if @bike
+  @bike = bike
 end
 
 #def capacity
